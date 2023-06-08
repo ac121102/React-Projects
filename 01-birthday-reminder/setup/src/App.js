c9754eba35936513e0a62acc529c60f2104826e3
Arvind <ac121102@gmail.com>
2023-06-08 19:03:01 +0530

    Birthday Project Completed

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+const handleClear = (setPeople, setPersonCount) => {
+  setPeople([]);
+  setPersonCount(0);
+};
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [personCount, setPersonCount] = useState(Object.keys(data).length);
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{personCount} birthdays today</h3>
+        <List list={people} />
+        <button onClick={() => handleClear(setPeople, setPersonCount)}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ list }) => {
+  console.log(list);
   return (
     <>
-      <h2>list component</h2>
+      {list.map(function (person) {
+        const { id, name, age, image } = person;
+        return (
+          <article
+            key={id}
+            className='person'
+          >
+            <img
+              src={image}
+              alt='image'
+            />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
