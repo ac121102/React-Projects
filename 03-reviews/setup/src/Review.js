43ba59654ee6662d6d45d0edf147bc53dc63d7ee
Arvind <ac121102@gmail.com>
2023-06-09 17:30:38 +0530

    Project-3 Reviews Completed

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,79 @@
 import React, { useState } from 'react';
 import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaQuoteRight,
+} from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  const revSize = people.length;
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      return (index - 1 + revSize) % revSize;
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      return (index + 1) % revSize;
+    });
+  };
+
+  const randomPerson = () => {
+    var newIndex = Math.floor(Math.random() * revSize);
+    if (index === newIndex) {
+      newIndex = (index + 1) % revSize;
+    }
+    setIndex(newIndex);
+  };
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img
+          src={image}
+          alt={name}
+          className='person-img'
+        />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+
+      <div className='button-container'>
+        <button
+          className='prev-btn'
+          onClick={prevPerson}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className='next-btn'
+          onClick={nextPerson}
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+
+      <div>
+        <button
+          className='random-btn'
+          onClick={randomPerson}
+        >
+          Surprise me!
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
