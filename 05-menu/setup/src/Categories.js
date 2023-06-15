1d02f8b95e4af9ddcf5237e9c184e73668a28574
Arvind <ac121102@gmail.com>
2023-06-15 17:30:35 +0530

    5-menu items completed

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  const toUpper = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            className='filter-btn'
+            key={index}
+            onClick={() => {
+              filterItems(category);
+            }}
+          >
+            {toUpper(category)}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
