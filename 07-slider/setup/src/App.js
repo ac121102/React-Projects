4961d455069ca0a6111f5801be856e2cf1618154
Arvind <ac121102@gmail.com>
2023-06-15 19:21:53 +0530

    cache

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import {
+  FiChevronRight,
+  FiChevronLeft,
+} from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
