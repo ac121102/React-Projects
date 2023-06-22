5f717fe7008689bbebbdec93968db539db8ee3cb
Arvind <ac121102@gmail.com>
2023-06-22 19:21:20 +0530

    08-Lorem Ipsum Done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -2,7 +2,54 @@ import React, { useState } from 'react';
 import data from './data';
 
 function App() {
-  return <h2>lorem ipsum project setup</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const repeat = (arr, n) => Array(n).fill(arr).flat();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let cnt = parseInt(count);
+    if (cnt < 1) {
+      cnt = 1;
+    }
+    setText(
+      repeat(data, Math.floor(cnt / data.length)).concat(
+        data.slice(0, cnt % data.length)
+      )
+    );
+  };
+
+  return (
+    <section className='section-center'>
+      <h3>Tired of boring Lorem ipsum?</h3>
+      <form
+        className='lorem-form'
+        onSubmit={handleSubmit}
+      >
+        <label className='amount'>Paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <br />
+        <button
+          type='submit'
+          className='btn'
+        >
+          Generate
+        </button>
+      </form>
+      <article className='lorem-text'>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
